309b7bd0642ffa07fd04e258acb7701d957f04b5
ijcastrosena <111159778+ijcastrosena@users.noreply.github.com>
2022-11-08 20:09:28 -0500

    Sesion 08-11-2022

---
+++ mern-ecommerce/backend/controllers/usuarioController.js
@@ -0,0 +1,11 @@
+
+
+const prueba = (req, res) => {
+    res.send({
+        msg: "En esta ruta gestionaremos todas las peticiones correspondiente al modelo de Usuarios"
+    })
+};
+
+export {
+    prueba
+};
